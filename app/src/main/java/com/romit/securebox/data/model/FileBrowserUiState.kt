79cff8e37b38a385885d5fab6c7cdf5074fea272
CodePandaaAI <romitsharmakv@gmail.com>
2025-11-17 20:22:33 +0530

    refactor: Minor UI and code cleanup on Home screen
    This commit introduces minor stylistic refinements to the Home screen and removes unused code for better maintainability.
    
    Key changes:
    - **`HomeScreen.kt`**:
        - The "Recents" and "Categories" section headers now use a custom font (`CustomFontFamily`) with a size of `24.sp` for a more consistent visual style.
        - Adjusted vertical spacing around the headers and sections for improved layout.
    
    - **`FileBrowserUiState.kt`**:
        - The unused import `android.os.Environment` has been removed to clean up the file.

--- app/src/main/java/com/romit/securebox/data/model/FileBrowserUiState.kt
+++ app/src/main/java/com/romit/securebox/data/model/FileBrowserUiState.kt
@@ -1,7 +1,5 @@
 package com.romit.securebox.data.model
 
-import android.os.Environment
-
 data class FileBrowserUiState(
     val browsingPath: String = "", // Current Path for browsing folders accessing allRecents/renaming/deleting/copying/Creating new folders and so on (For Navigation and general purpose work)
     val browsingPathDirectories: List<FileItem> = emptyList(), // Items(Dir) inside browsing path
